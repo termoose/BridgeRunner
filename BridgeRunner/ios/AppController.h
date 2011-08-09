af13ad2ee84355dfb862124a4cf37031035e6275
Ole Andre Birkedal <birkedal85@gmail.com>
2011-08-09 22:41:26 +0200

    Initial Commit

---
+++ BridgeRunner/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  BridgeRunnerAppController.h
+//  BridgeRunner
+//
+//  Created by Ole Andre Birkedal on 8/9/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController	*viewController;
+}
+
+@end
+
---
+++ BridgeRunner/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  BridgeRunnerAppController.h
+//  BridgeRunner
+//
+//  Created by Ole Andre Birkedal on 8/9/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
